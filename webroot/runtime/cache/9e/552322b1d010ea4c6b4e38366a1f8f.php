53575d9ce74befa52718f87c29a5f0b0002c6ee2
MrTryAgain <z1176814811@gmail.com>
2020-08-20 14:40:28 +0800

    no message

---
+++ webroot/runtime/cache/9e/552322b1d010ea4c6b4e38366a1f8f.php
@@ -0,0 +1,4 @@
+<?php
+//000000000600
+ exit();?>
+s:203:"/zpadmin.php/admin/collect/api.html?ac=cj&cjflag=e1330d402d849fe99b7f608a71cb8cdc&cjurl=http%3A%2F%2Fcaiji.kuyun98.com%2Finc%2Fldg_api.php&h=24&t=&ids=&wd=&type=1&mid=1&opt=0&filter=0&filter_from=&param=";
---
+++ webroot/static/js/tj.js
@@ -0,0 +1,10 @@
+document.write('<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?313f52b829ff9517c936da394114643f";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+')
